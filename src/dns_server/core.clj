0c2601de6b2b697bb9a4aa2d454023f411533b07
Jeremy Gailor <jgailor@gmail.com>
2014-12-20 18:35:36 -0800

    Lot of work on a plane. Had to reverse engineer the question section format a bit, but was able to figure out the domain names as well as some of the message type information (mx, cname, a-record... this isn't here yet, but coming soon)

--- src/dns_server/core.clj
+++ src/dns_server/core.clj
@@ -13,6 +13,16 @@
         (byte-array)
         (biginteger))))
 
+(defn hash-entry-to-string
+  "Converts a hash entry to printable string"
+  [entry width]
+  (str (format (str "%-" width "s") (str (name (first entry)) ": ")) (second entry)))
+
+(defn hash-to-string
+  "Converts a hash to a printable string"
+  [hashy-thing width]
+  (clojure.string/join "\n" (map #(hash-entry-to-string % width) hashy-thing)))
+
 (defn send
   "Send a short textual message over a DatagramSocket to the specified
    host and port. If the string is over 512 bytes long, it will be
@@ -36,22 +46,62 @@
 (defn process-headers
   "Process the 12-byte header into the appropriate fields"
   [header]
-  (let [id-field (bytes->int (byte-array (take 2 header)))
-        qr-field (bit-and 1 (first (drop 2 header)))
-        opcode-field (bit-and 15 (bit-shift-right (first (drop 2 header)) 1))]
-    (hash-map :id-field id-field :qr-field qr-field :opcode-field opcode-field)))
+  (let [id-field     (bytes->int (byte-array (take 2 header)))
+        byte3        (first (drop 2 header))
+        byte4        (first (drop 3 header))
+        qr-field     (bit-and 2r00000001 byte3)
+        opcode-field (bit-and 2r00011110 byte3)
+        aa-field     (bit-and 2r00100000 byte3)
+        tc-field     (bit-and 2r01000000 byte3)
+        rd-field     (bit-and 2r10000000 byte3)
+        ra-field     (bit-and 2r00000001 byte4)
+        z-field      (bit-and 2r00001110 byte4)
+        rcode-field  (bit-and 2r11110000 byte4)
+        qd-count     (bytes->int (byte-array (take 2 (drop 4 header))))
+        an-count     (bytes->int (byte-array (take 2 (drop 6 header))))
+        ns-count     (bytes->int (byte-array (take 2 (drop 8 header))))
+        ar-count     (bytes->int (byte-array (take 2 (drop 10 header))))]
+    (hash-map :id-field id-field
+              :qr-field qr-field
+              :opcode-field opcode-field
+              :aa-field aa-field
+              :tc-field tc-field
+              :rd-field rd-field
+              :ra-field ra-field
+              :z-field z-field
+              :rcode-field rcode-field
+              :qd-count qd-count
+              :an-count an-count
+              :ns-count ns-count
+              :ar-count ar-count)))
+
+(defn domain-length
+  "Returns the length of bytes of the domain"
+  ([bytes] (domain-length bytes 0))
+  ([bytes length] (if (= 0 (first bytes) (second bytes)) length (domain-length (rest bytes) (inc length)))))
+
+(defn process-domain
+  "Converts the '.' offset + ascii-chars to fully-qualified domain name"
+  [bytes]
+  (if (empty? bytes)
+    []
+    (let [offset (first bytes)
+          remainder (rest bytes)]
+      (concat (take offset remainder) (list (byte \.)) (process-domain (drop offset remainder))))))
 
 (defn process-questions
   "Process the variable length question field from the DNS request"
   [questions]
-  (String. (byte-array questions) 0 (count questions)))
+  (let [domain-bytes (domain-length questions)
+        full-domain (process-domain (take domain-bytes questions))]
+    (hash-map :domain (String. (byte-array full-domain)))))
 
 (defn process-data
   "Processes a DNS request into a header and a question"
   [data]
-  (let [header (take 12 data)
-        questions (drop 12 data)]
-    (hash-map :headers (process-headers header) :questions (process-questions questions))))
+  (let [headers (process-headers (take 12 data))
+        questions (process-questions (drop 12 data))]
+    (hash-map :headers headers :questions questions)))
 
 (defn receive-loop
   "Given a function and DatagramSocket, will (in another thread)
@@ -64,8 +114,9 @@
   "Prints out the data from the packet for debugging purposes"
   [data]
   (let [{headers :headers questions :questions} (process-data data)
-        output (map (fn [entry] (str (name (first entry)) ": " (second entry))) headers)]
-    (println (str (clojure.string/join "\n" output) "\n" questions "\n"))))
+        header-output (hash-to-string headers 15)
+        questions-output (hash-to-string questions 15)]
+    (println (str header-output "\n" questions-output "\n"))))
 
 (defn -main
   "Kicks this pig"
